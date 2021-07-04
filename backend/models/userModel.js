175f0e688daa0f8cba3b48668270dafd33bc80ea
Harsh Shah <harshsh31@gmail.com>
2021-07-04 15:38:04 +0530

    added user authentication and authorizations

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,14 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bycrpt.compare(enteredPassword, this.password);
 };
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bycrpt.genSalt(10);
+  this.password = await bycrpt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
